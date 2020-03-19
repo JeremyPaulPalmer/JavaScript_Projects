5757023d8c3463125478bd7ecd5eab7f8a3bd11f
Paseo92 <duxfan18@gmail.com>
2020-03-18 23:28:59 -0700

    Add Basic JavaScript Projects folder
    Added Basic JavaScript Projects folder which contains Basic_HTML_1.html and Basic_JavaScript_1.js

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+var players = "Ducks", sabrina = "Ionescu", peyton = "Pritchard";
+var players = players.fontcolor("yellow");
+var sabrina = sabrina.fontcolor("green");
+var peyton = peyton.fontcolor("grey");
+document.write(players);
+document.write(sabrina);
+document.write(peyton);
+document.write(5 * 2);
